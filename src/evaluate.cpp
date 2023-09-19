85b16761b358b52d2557a67dee269ed877ee7d10
ThomasLEMERCIER <t.lemercier27@gmail.com>
2023-09-19 13:22:45 +0200

    Add tapered PSQT and tapered material evaluation.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -4,19 +4,33 @@ Score evaluate(const Position &position) {
     EvaluationData evaluationData{};
     initializeEvaluationData(position, evaluationData);
 
-    Score materialScore = evaluateMaterial(evaluationData);
-    Score pawnScore = evaluatePawns<Color::White>(position) - evaluatePawns<Color::Black>(position);
-    Score pieceScore =
+    ScoreExt materialScore = evaluateMaterial(evaluationData);
+    ScoreExt pawnScore = evaluatePawns<Color::White>(position) - evaluatePawns<Color::Black>(position);
+    ScoreExt pieceScore =
             evaluatePieces<PieceType::Knight, Color::White>(position) - evaluatePieces<PieceType::Knight, Color::Black>(position) +
             evaluatePieces<PieceType::Bishop, Color::White>(position) - evaluatePieces<PieceType::Bishop, Color::Black>(position) +
             evaluatePieces<PieceType::Rook, Color::White>(position) - evaluatePieces<PieceType::Rook, Color::Black>(position) +
             evaluatePieces<PieceType::Queen, Color::White>(position) - evaluatePieces<PieceType::Queen, Color::Black>(position);
-    Score kingScore = evaluateKing<Color::White>(position) - evaluateKing<Color::Black>(position);
+    ScoreExt kingScore = evaluateKing<Color::White>(position) - evaluateKing<Color::Black>(position);
 
-    Score finalScore = materialScore + pawnScore + pieceScore + kingScore;
+    ScoreExt finalScore = materialScore + pawnScore + pieceScore + kingScore;
 
-    finalScore = (position.sideToMove == Color::White) ? finalScore : -finalScore;
-    return finalScore;
+    evaluatePhase(evaluationData);
+    Score evaluation = interpolateScore(finalScore, evaluationData);
+
+    evaluation = (position.sideToMove == Color::White) ? evaluation : -evaluation;
+    return evaluation;
+}
+
+void evaluatePhase(EvaluationData& evaluationData) {
+    evaluationData.phase = (evaluationData.whiteKnightCount + evaluationData.blackKnightCount) * knightPhaseValue
+                         + (evaluationData.whiteBishopCount + evaluationData.blackBishopCount) * bishopPhaseValue
+                         + (evaluationData.whiteRookCount + evaluationData.blackRookCount) * rookPhaseValue
+                         + (evaluationData.whiteQueenCount + evaluationData.blackQueenCount) * queenPhaseValue;
+}
+
+Score interpolateScore(ScoreExt finalScore, EvaluationData& evaluationData) {
+    return  (finalScore.mg * evaluationData.phase + finalScore.eg * (phaseMidGame - evaluationData.phase)) / phaseMidGame;
 }
 
 void initializeEvaluationData(const Position& position, EvaluationData& evaluationData) {
@@ -33,8 +47,8 @@ void initializeEvaluationData(const Position& position, EvaluationData& evaluati
     evaluationData.blackQueenCount = position.black.queens.count();
 }
 
-Score evaluateMaterial(EvaluationData& evaluationData) {
-    Score score = (evaluationData.whitePawnCount - evaluationData.blackPawnCount) * pawnValue
+ScoreExt evaluateMaterial(EvaluationData& evaluationData) {
+    ScoreExt score = (evaluationData.whitePawnCount - evaluationData.blackPawnCount) * pawnValue
                 + (evaluationData.whiteKnightCount - evaluationData.blackKnightCount) * knightValue
                 + (evaluationData.whiteBishopCount - evaluationData.blackBishopCount) * bishopValue
                 + (evaluationData.whiteRookCount - evaluationData.blackRookCount) * rookValue
@@ -44,11 +58,11 @@ Score evaluateMaterial(EvaluationData& evaluationData) {
 }
 
 template <Color color>
-Score evaluatePawns(const Position &position) {
+ScoreExt evaluatePawns(const Position &position) {
     constexpr Piece ourPiece = (color == Color::White) ? Piece::WhitePawn : Piece::BlackPawn;
     Bitboard ourPawns = position.getPieces<ourPiece>();
 
-    Score score{};
+    ScoreExt score{};
     while (ourPawns) {
         Square square = ourPawns.popLsb();
 
@@ -60,11 +74,11 @@ Score evaluatePawns(const Position &position) {
 }
 
 template<PieceType pieceType, Color color>
-Score evaluatePieces(const Position& position) {
+ScoreExt evaluatePieces(const Position& position) {
     constexpr Piece piece = getPiece(pieceType, color);
     Bitboard pieceBitboard = position.getPieces<piece>();
 
-    Score score{};
+    ScoreExt score{};
     while (pieceBitboard) {
         Square square = pieceBitboard.popLsb();
 
@@ -76,12 +90,12 @@ Score evaluatePieces(const Position& position) {
 }
 
 template<Color color>
-Score evaluateKing(const Position &position) {
+ScoreExt evaluateKing(const Position &position) {
     Bitboard kingBitboard = (color == Color::White) ? position.white.king : position.black.king;
     Square kingSquare = kingBitboard.popLsb();
 
     // PSQT
-    Score score = (color == Color::White) ? kingSquareTable[kingSquare.index()] : kingSquareTable[kingSquare.flipIndex()];
+    ScoreExt score = (color == Color::White) ? kingSquareTable[kingSquare.index()] : kingSquareTable[kingSquare.flipIndex()];
 
     return score;
 }
