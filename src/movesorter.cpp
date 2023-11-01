919c96f9946b94a0dc4616583a5245da246069a1
ThomasLEMERCIER <t.lemercier27@gmail.com>
2023-11-01 11:21:23 +0100

    SEE move ordering

--- src/movesorter.cpp
+++ src/movesorter.cpp
@@ -2,8 +2,11 @@
 
 #include "movegen.hpp"
 #include "piece.hpp"
+#include "see.hpp"
 
 bool MoveSorter::nextMove(Move& outMove, bool skipQuiet) {
+    std::uint32_t bestIndex = MoveSorter::InvalidIndex;
+
     switch (currentStage) {
         case MoveSorterStage::TTMove:
             currentStage = MoveSorterStage::GeneratingNonQuiets;
@@ -17,80 +20,114 @@ bool MoveSorter::nextMove(Move& outMove, bool skipQuiet) {
             moveList.filter(ttMove);
             scoreNonQuiets();
 
-            currentStage = MoveSorterStage::NonQuiets;
+            currentStage = MoveSorterStage::GoodNonQuiets;
             [[fallthrough]];
-        case MoveSorterStage::NonQuiets:
-            if (indexMoveList < moveList.getSize()) {
-                outMove = nextSortedMove();
+        case MoveSorterStage::GoodNonQuiets:
+            bestIndex = nextSortedIndex<Filter::BadNonQuiets>(currentIndex, moveList.getSize());
+
+            // no more good non quiets
+            if (bestIndex == MoveSorter::InvalidIndex) {
+                badNonQuietsIndex = currentIndex;
+            }
+            // good non-quiet
+            else {
+                outMove = pop(bestIndex);
+                return true;
+            }
+
+            [[fallthrough]];
+        case MoveSorterStage::GeneratingQuiets:
+            quietMoveIndex = moveList.getSize();
+            if (!skipQuiet) {
+                generateMoves<MoveType::QuietMoves>(moveList, position);
+                moveList.filter(ttMove);
+            }
+
+            [[fallthrough]];
+        case MoveSorterStage::Killer1:
+            if (!skipQuiet) {
+                currentStage = MoveSorterStage::Killer2;
+                if (moveList.filter(killerMoves.killer1)) {
+                    outMove = killerMoves.killer1;
+                    return true;
+                }
+            }
+
+            [[fallthrough]];
+        case MoveSorterStage::Killer2:
+            if (!skipQuiet) {
+                currentStage = MoveSorterStage::OrderingQuiets;
+                if (moveList.filter(killerMoves.killer2)) {
+                    outMove = killerMoves.killer2;
+                    return true;
+                }
+            }
+
+            [[fallthrough]];
+        case MoveSorterStage::OrderingQuiets:
+            if (!skipQuiet) {
+                scoreQuiets();
+                currentStage = MoveSorterStage::Quiets;
+            }
+
+            [[fallthrough]];
+        case MoveSorterStage::Quiets:
+            if (!skipQuiet && currentIndex < moveList.getSize()) {
+                bestIndex = nextSortedIndex<Filter::None>(currentIndex, moveList.getSize());
+                outMove = pop(bestIndex);
                 return true;
             }
             else {
-                if (skipQuiet) return false;
-                currentStage = MoveSorterStage::GeneratingQuiets;
+                currentStage = MoveSorterStage::BadNonQuiets;
+                currentIndex = badNonQuietsIndex;
             }
             [[fallthrough]];
-        case MoveSorterStage::GeneratingQuiets:
-            generateMoves<MoveType::QuietMoves>(moveList, position);
-            moveList.filter(ttMove);
-
-            currentStage = MoveSorterStage::Killer1;
-            [[fallthrough]];
-        case MoveSorterStage::Killer1:
-            currentStage = MoveSorterStage::Killer2;
-            if (moveList.filter(killerMoves.killer1)) {
-                outMove = killerMoves.killer1;
-                return true;
-            }
-            [[fallthrough]];
-        case MoveSorterStage::Killer2:
-            currentStage = MoveSorterStage::OrderingQuiets;
-            if (moveList.filter(killerMoves.killer2)) {
-                outMove = killerMoves.killer2;
-                return true;
-            }
-            [[fallthrough]];
-        case MoveSorterStage::OrderingQuiets:
-            scoreQuiets();
-
-            currentStage = MoveSorterStage::Quiets;
-            [[fallthrough]];
-        case MoveSorterStage::Quiets:
-            if (!skipQuiet && indexMoveList < moveList.getSize()) {
-                outMove = nextSortedMove();
+        case MoveSorterStage::BadNonQuiets:
+            if (currentIndex < quietMoveIndex) {
+                bestIndex = nextSortedIndex<Filter::None>(currentIndex, quietMoveIndex);
+                outMove = pop(bestIndex);
                 return true;
             }
             else {
                 return false;
             }
-        default:
-            return false;
     }
+    return false;
 }
 
 void MoveSorter::scoreNonQuiets() {
-    for (std::uint32_t count = indexMoveList; count < moveList.getSize(); ++count) {
-        PieceType attacker = getPieceType(moveList[count].move.getPiece());
-        PieceType victim = getPieceType(position.pieceAt(moveList[count].move.getTo()));
+    for (std::uint32_t index = currentIndex; index < moveList.getSize(); ++index) {
+        PieceType attacker = getPieceType(moveList[index].move.getPiece());
+        PieceType victim = getPieceType(position.pieceAt(moveList[index].move.getTo()));
 
-        moveList[count].score = 6 * static_cast<std::int32_t>(victim) - static_cast<std::int32_t>(attacker);
+        moveList[index].score = 6 * static_cast<std::int32_t>(victim) - static_cast<std::int32_t>(attacker);
     }
 }
 
 void MoveSorter::scoreQuiets() {
-    for (std::uint32_t count = indexMoveList; count < moveList.getSize(); ++count) {
-        const Move& move = moveList[count].move;
-        moveList[count].score = quietHistoryTable[static_cast<std::uint8_t>(position.sideToMove)][move.getFrom().index()][move.getTo().index()];
+    for (std::uint32_t index = quietMoveIndex; index < moveList.getSize(); ++index) {
+        const Move& move = moveList[index].move;
+        moveList[index].score = quietHistoryTable[static_cast<std::uint8_t>(position.sideToMove)][move.getFrom().index()][move.getTo().index()];
     }
 }
 
-Move MoveSorter::nextSortedMove() {
-    std::uint32_t bestIndex = indexMoveList;
-    for (std::uint32_t index = indexMoveList + 1; index < moveList.getSize(); index++) {
-        if (moveList[index].score > moveList[bestIndex].score) bestIndex = index;
-    }
+Move MoveSorter::pop(std::uint32_t index) {
+    std::swap<MoveExt>(moveList[index], moveList[currentIndex]);
+    return moveList[currentIndex++].move;
+}
 
-    MoveExt temp = moveList[indexMoveList];
-    moveList[indexMoveList] = moveList[bestIndex];
-    moveList[bestIndex] = temp;
-    return moveList[indexMoveList++].move;
+template<Filter filter>
+std::uint32_t MoveSorter::nextSortedIndex(std::uint32_t start, std::uint32_t end) {
+    std::uint32_t bestIndex = MoveSorter::InvalidIndex;
+    for (std::uint32_t index = start; index < end; index++) {
+        if (bestIndex == MoveSorter::InvalidIndex || moveList[index].score > moveList[bestIndex].score) {
+            if constexpr (filter == Filter::BadNonQuiets) {
+                if (!staticExchangeEvaluation(position, moveList[index].move, MoveSorter::GoodNonQuietThreshold)) {
+                    continue;
+                }
+            }
+            bestIndex = index;
+        }
+    }
+    return bestIndex;
 }
